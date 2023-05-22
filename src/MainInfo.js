b02dcb6b388f4268d46da95f22544020efb58084
Brady Moore <banditomans@gmail.com>
2023-05-22 00:25:25 -0500

    Beta 1.0

--- src/MainInfo.js
+++ src/MainInfo.js
@@ -3,7 +3,7 @@ import './App.css';
 import Nav from './Nav';
 import Data from './data.json';
 import { useParams } from 'react-router-dom';
-import btm from "./img/bradytmoore.jpg";
+import btm from "./img/aboutpic.jpg";
 import btm2 from "./img/bradytmoore2.jpg";
 import lcspf from "./img/lcspf.jpg";
 import compinven from "./img/companyinventory.png";
@@ -14,6 +14,11 @@ import playlist from "./img/playlistprogram.png";
 import resume from "./img/btmresume.pdf";
 import resumeIcon from "./img/file-solid.svg";
 import globe from "./img/planet-earth.png";
+import css from "./img/css3-alt.svg";
+import html from "./img/html5.svg";
+import javascript from "./img/js.svg";
+import java from "./img/java.svg";
+import react from "./img/react.svg";
 
 
 
@@ -25,10 +30,7 @@ function MainInfo() {
 
     return (
     <div id="MainContent">
-        <div className="ContentArea">
-        {/* <img id="bradypic" src={Data[0].picture} /> */}
-        <img className="bradypic" alt="Brady Moore" src={btm} />
-        </div>
+        
         <div className="ContentArea">
         <h1 className="MainDetails">Brady Moore</h1>
         <h1 className="MainDetails">Web Developer/Software Engineer</h1>
@@ -49,7 +51,7 @@ function MainInfo() {
                     <br/>
                 </div>
                 <div className="AboutMeContent">
-                    <img className="AboutMePic" alt="Brady Moore" src={btm2} />
+                    <img className="AboutMePic" alt="Brady Moore" src={btm} />
 
                 </div>
                 <div className="AboutMeContent2">
@@ -63,11 +65,11 @@ function MainInfo() {
                     <h3>GPA: 3.31</h3>
                     <br/>
                     <h2>Main Profeciencies: </h2>
-                    <img className="Icons" alt="React" src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" />
-                    <img className="Icons" alt="html5" src="https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white" />
-                    <img className="Icons" alt="JavaScript" src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" />
-                    <img className="Icons" alt="CSS" src="https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white" />
-                    <img className="Icons" alt="Java" src="https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white" />
+                    <img className="Icons" alt="React" src={react} />
+                    <img className="Icons" alt="html5" src={html} />
+                    <img className="Icons" alt="JavaScript" src={javascript} />
+                    <img className="Icons" alt="CSS" src={css} />
+                    <img className="Icons" alt="Java" src={java} />
 
                     <br/>
                     <br/>
@@ -82,8 +84,16 @@ function MainInfo() {
                     <br/>
                     <br/>
                     <br/>
+                    
 
                 </div>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+
         </div>
 
 
@@ -96,50 +106,61 @@ function MainInfo() {
             <br/>
             <br/>
             <div className="ProjectContent">
-                <img className="Icons" alt="React" src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" />
-                <img className="Icons" alt="html5" src="https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white" />
-                <img className="Icons" alt="JavaScript" src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" />
-                <img className="Icons" alt="CSS" src="https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white" />
+                <img className="Icons" alt="React" src={react} />
+                <img className="Icons" alt="html5" src={html} />
+                <img className="Icons" alt="JavaScript" src={javascript} />
+                <img className="Icons" alt="CSS" src={css} />
 
 
                 <h1>LCS Player Finder</h1>
+                <a href="https://lcsapp-74977.web.app/">
                 <img className="bradypic2" alt="LCS Player Finder Picture" src={lcspf} />
-                
+                </a>
 
             </div>
             <div className="ProjectContent">
-            <img className="Icons" alt="Java" src="https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white" />
+            <img className="Icons" alt="Java" src={java} />
 
                 <h1>Company Inventory Program</h1>
+                <a href="https://github.com/bradytmoore/Company-Inventory-Program">
                 <img className="bradypic2" alt="Company Inventory Program" src={compinven} />
+                </a>
 
             </div>
             <div className="ProjectContent">
-            <img className="Icons" alt="Java" src="https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white" />
+            <img className="Icons" alt="Java" src={java} />
 
                 <h1>Triple Encryption</h1>
+                <a href="https://github.com/bradytmoore/Triple-Encryption">
                 <img className="bradypic2" alt="Triple Encryption Program" src={compsec} />
+                </a>
 
             </div>
             <div className="ProjectContent">
-            <img className="Icons" alt="Java" src="https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white" />
+            <img className="Icons" alt="Java" src={java} />
 
                 <h1>Huffman Encoding</h1>
+                <a href="https://github.com/bradytmoore/Huffman-Encoding-Program">
                 <img className="bradypic2" alt="Huffman Encoding Program" src={huffenc} />
-
+                </a>
             </div>
             <div className="ProjectContent">
-            <img className="Icons" alt="Java" src="https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white" />
+            <img className="Icons" alt="Java" src={java} />
 
                 <h1>Password Manager</h1>
+                <a href="https://github.com/bradytmoore/Password-Manager">
                 <img className="bradypic2" alt="Password Manager Program" src={passpro} />
+                </a>
 
             </div>
             <div className="ProjectContent">
-            <img className="Icons" alt="Java" src="https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white" />
+            <img className="Icons" alt="Java" src={java} />
 
                 <h1>Playlist Program</h1>
+
+                <a href="https://github.com/bradytmoore/Playlist-Program">
                 <img className="bradypic2" alt="Playlist Program" src={playlist} />
+                </a>
 
             </div>
         </div>
