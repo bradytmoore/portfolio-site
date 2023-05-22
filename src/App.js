1b23546ed86e491614502df96eeaa55ffd790ffe
Brady Moore <banditomans@gmail.com>
2023-05-22 01:06:58 -0500

    Fixed Deployment

--- src/App.js
+++ src/App.js
@@ -3,47 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './Nav';
 import MainInfo from './MainInfo';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
 import linkedin from "./img/linkedin.png";
 import github from "./img/github.png";
 
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Nav/>
-      </div>
+    <HashRouter>
+      <Router>
+        <div className="App">
+          <Nav/>
+        </div>
 
-      <Routes>
-        <Route path="/" element={<MainInfo /> }/>
-      </Routes>
-      <br/>
-      <br/>
+        <Routes>
+          <Route path="/" element={<MainInfo /> }/>
+        </Routes>
+        <br/>
+        <br/>
 
-      <footer>
-        <ul className="Footer">
-          <h1>Contact Me:</h1>
-            <li>
-                brady.moore112@gmail.com
-            </li>
-            <li>
-                769-232-0388
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/brady-moore-916531229/">
-                <img alt="LinkedIn Icon by Freepik" className="FootIco" src={linkedin}/>
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/bradytmoore">
-                  <img alt="Github Icon By IconsBox" className="FootIco" src={github}/>
-              </a>
-            </li>
-            
-        </ul>
-      </footer>
-    </Router>
+        <footer>
+          <ul className="Footer">
+            <h1>Contact Me:</h1>
+              <li>
+                  brady.moore112@gmail.com
+              </li>
+              <li>
+                  769-232-0388
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/brady-moore-916531229/">
+                  <img alt="LinkedIn Icon by Freepik" className="FootIco" src={linkedin}/>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/bradytmoore">
+                    <img alt="Github Icon By IconsBox" className="FootIco" src={github}/>
+                </a>
+              </li>
+              
+          </ul>
+        </footer>
+      </Router>
+    </HashRouter>
 
 
   );
